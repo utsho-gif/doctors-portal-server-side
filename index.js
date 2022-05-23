8d0852625aef2e9ddd32007f6b3f5b819358925b
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-05-24 03:34:51 +0600

    added admin role for individual user

--- index.js
+++ index.js
@@ -94,6 +94,39 @@ async function run(){
             return res.send({success:true, result});
         })
 
+        //get all user 
+        app.get('/user',verifyJWT, async(req,res) => {
+            const user = await userCollection.find().toArray();
+            res.send(user);
+        })
+
+        //cheking admin or not
+        app.get('/admin/:email',async(req,res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin});
+        })
+
+        //make an admin
+        app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: requester});
+            if(requesterAccount.role === 'admin'){
+                const filter = { email: email };
+                const updateDoc = {
+                 $set: {role: 'admin'},
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else{
+                return res.status(403).send({message: 'Forbidden Aceess'});
+            }
+            
+          })
+
         //update a user 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
